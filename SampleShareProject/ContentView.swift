09d93b7f6a31613a8d4ed6a4ba08ad2fa481d6b5
KaitoMuraoka <kaitoboshi@icloud.com>
2024-07-13 10:49:36 +0900

    Create Sharelink iOS 16 or later

--- SampleShareProject/ContentView.swift
+++ SampleShareProject/ContentView.swift
@@ -3,6 +3,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            // iOS 16
+            let shareText = "iOS 16 or later shareItem"
+            ShareLink(item: shareText) {
+                HStack {
+                    Image(systemName: "square.and.arrow.up")
+                    Text("iOS 16 or later")
+                }
+            }
+            
             // iOS 15
             Button {
                 let shareText = "iOS 15 or earlier shareItem"
