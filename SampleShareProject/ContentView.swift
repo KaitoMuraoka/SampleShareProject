a19827b87ad0b9b962279e64b49a4628fb0d110d
KaitoMuraoka <kaitoboshi@icloud.com>
2024-07-13 10:08:47 +0900

    Initial Commit

---
+++ SampleShareProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleShareProject
+//
+//  Created by 村岡海人 on 2024/07/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleShareProject/SampleShareProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleShareProjectApp.swift
+//  SampleShareProject
+//
+//  Created by 村岡海人 on 2024/07/13.
+//
+
+import SwiftUI
+
+@main
+struct SampleShareProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
